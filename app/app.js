1d56d468df3f9ecf1c1004c485f9b88f6183a20e
Luquinhas <lucmsilva651@gmail.com>
2025-04-22 00:59:47 -0300

    First commit (WIP)

---
+++ app/app.js
@@ -0,0 +1,69 @@
+import { ChiptuneJsPlayer as chiptune3 } from 'https://DrSnuggles.github.io/chiptune/chiptune3.js';
+import { dnd } from "https://DrSnuggles.github.io/chiptune/dnd.js";
+
+const modProgress = document.getElementById("modProgress");
+const ppToggle = document.getElementById("ppToggle");
+const stopBtn = document.getElementById("stopBtn");
+const modTitle = document.getElementById("modTitle");
+const modDur = document.getElementById("modDur");
+const modType = document.getElementById("modType");
+const toolbarCenter = document.getElementById("toolbarCenter");
+const modTracker = document.getElementById("modTracker");
+
+function fmtMSS(seconds) {
+  return (seconds - (seconds %= 60)) / 60 + (9 < seconds ? ":" : ":0") + seconds
+}
+
+Number.prototype.round = function () {
+  return Math.round(this);
+}
+
+function intToFloat(num, decPlaces) {
+  return num.toFixed(decPlaces);
+}
+
+window.chiplib = new chiptune3();
+
+chiplib.onInitialized(() => {
+  chiplib.setRepeatCount(0);
+  dnd(window, (file) => {
+    chiplib.play(file);
+  });
+})
+
+chiplib.onError((err) => {
+  if (err.type === "ptr") {
+    alertError("Unknown error, but it's probably a bad URL or ID.");
+  } else if (err.type === "Load") {
+    alertError("Failed to load the module. (?)");
+  } else {
+    alertError(err.type);
+  };
+  chiplib.stop();
+});
+
+let actualDur = 0;
+chiplib.onMetadata(async (meta) => {
+  const modDurStr = fmtMSS(meta.dur.round()) || "0:00";
+  const modTypeStr = meta.type.toUpperCase() || "Unknown";
+  actualDur = meta.dur.round();
+  modTitle.innerText = meta.title || "Untitled";
+  modType.innerText = modTypeStr;
+  modDur.innerText = modDurStr
+  modProgress.width = `${Number(meta.dur.round())}vw`;
+  modTracker.innerText = meta.tracker || "Unknown";
+});
+
+document.body.onkeyup = function(btn) {
+  if (btn.key == " " || btn.code == "Space") {
+    chiplib.togglePause();
+  };
+};
+
+ppToggle.addEventListener("click", () => {
+  chiplib.togglePause();
+});
+
+stopBtn.addEventListener("click", () => {
+  chiplib.stop();
+});
---
+++ main.js
@@ -0,0 +1,51 @@
+import { app, BrowserWindow, globalShortcut } from "electron";
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+app.commandLine.appendSwitch('autoplay-policy', 'no-user-gesture-required');
+
+app.whenReady().then(() => {
+  const win = new BrowserWindow({
+    width: 600,
+    height: 480,
+    resizable: false,
+    maximizable: false,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+      sandbox: true,
+      // devTools: false,
+    },
+    ...(process.platform !== 'darwin' ? {
+      titleBarStyle: 'hidden',
+      titleBarOverlay: {
+        color: '#131313',
+        symbolColor: '#ffffff',
+        height: 30
+      },
+    } : {}),
+  });
+
+  app.on('browser-window-focus', function () {
+    globalShortcut.register("CommandOrControl+R", () => { return false });
+    globalShortcut.register("F5", () => { return false });
+  });
+
+  app.on('browser-window-blur', function () {
+    globalShortcut.unregister('CommandOrControl+R');
+    globalShortcut.unregister('F5');
+  });
+
+  win.loadFile(join(__dirname, 'app/app.html'));
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+
+  app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") app.quit();
+  });
+});
