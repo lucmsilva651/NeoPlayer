a5cae0ed91eb76e997fe13087bce87ffa65b4dd7
ukriu <80760888+ukriu@users.noreply.github.com>
2025-04-24 04:28:19 +0530

    Improve ux (#4)
    * app: css: dynamic footer
    
    * app: js: css: add animations and better layout
    
    * app: css: footer

--- app/app.js
+++ app/app.js
@@ -4,62 +4,55 @@ import { dnd } from "https://DrSnuggles.github.io/chiptune/dnd.js";
 const play = document.getElementById("play");
 const url = document.getElementById("url");
 const playBtn = document.getElementById("playBtn");
-const pauseBtn = document.getElementById("pauseBtn");
 const stopBtn = document.getElementById("stopBtn");
+const loopToggle = document.getElementById("loopToggle");
 const modTitle = document.getElementById("modTitle");
 const modDur = document.getElementById("modDur");
 const modType = document.getElementById("modType");
 const modTracker = document.getElementById("modTracker");
 const moduleMsgBtn = document.getElementById("moduleMsgBtn");
-const loopToggle = document.getElementById("loopToggle");
-const modDetails = document.getElementById("modDetails");
 const closeBtn = document.getElementById("closeBtn");
 const minimizeBtn = document.getElementById("minimizeBtn");
 const maximizeBtn = document.getElementById("maximizeBtn");
 
 let modMeta = "";
+let loopState = -1;  // 0 for off, -1 for loop
 
-const modulePage1 = "modarchive.org/index.php?request=view_by_moduleid"
-const modulePage2 = "modarchive.org/index.php?request=view_player"
-const modulePage3 = "modarchive.org/module.php"
-const apiDownload = "https://api.modarchive.org/downloads.php?moduleid="
+const modulePage1 = "modarchive.org/index.php?request=view_by_moduleid";
+const modulePage2 = "modarchive.org/index.php?request=view_player";
+const modulePage3 = "modarchive.org/module.php";
+const apiDownload = "https://api.modarchive.org/downloads.php?moduleid=";
 
 function moduleMsg(msg) {
   window.api.openDialog(msg);
 }
 
 function updateMaximizeIcon(isMaximized) {
-  if (isMaximized) {
-    maximizeBtn.classList.remove("ms-Icon--SquareShape");
-    maximizeBtn.classList.add("ms-Icon--ChromeRestore");
-  } else {
-    maximizeBtn.classList.remove("ms-Icon--ChromeRestore");
-    maximizeBtn.classList.add("ms-Icon--SquareShape");
-  }
+  maximizeBtn.classList.toggle("ms-Icon--SquareShape", !isMaximized);
+  maximizeBtn.classList.toggle("ms-Icon--ChromeRestore", isMaximized);
 }
 
 function fmtMSS(seconds) {
-  return (seconds - (seconds %= 60)) / 60 + (9 < seconds ? ":" : ":0") + seconds
+  return (seconds - (seconds %= 60)) / 60 + (9 < seconds ? ":" : ":0") + seconds;
 }
 
 Number.prototype.round = function () {
   return Math.round(this);
-}
+};
 
 function alertError(error) {
   alert(`Error: ${error}`);
   hideElements();
-  return;
 }
 
 function hideElements() {
-  modDetails.style.display = "none";
-  moduleMsgBtn.style.display = "none";
+  modDetails.classList.remove("show");
+  moduleMsgBtn.classList.remove("show");
 }
 
 function showElements() {
-  modDetails.style.display = "initial";
-  moduleMsgBtn.style.display = "initial";
+  modDetails.classList.add("show");
+  moduleMsgBtn.classList.add("show");
 }
 
 window.api.onWindowMaximized((isMaximized) => {
@@ -73,11 +66,9 @@ document.addEventListener("DOMContentLoaded", () => {
 window.chiplib = new chiptune3();
 
 chiplib.onInitialized(() => {
-  chiplib.setRepeatCount(-1);
-  dnd(window, (file) => {
-    chiplib.play(file);
-  });
-})
+  chiplib.setRepeatCount(loopState);
+  dnd(window, (file) => chiplib.play(file));
+});
 
 chiplib.onError((err) => {
   if (err.type === "ptr") {
@@ -86,13 +77,13 @@ chiplib.onError((err) => {
     alertError("Failed to load the module. (?)");
   } else {
     alertError(err.type);
-  };
+  }
   chiplib.stop();
 });
 
 chiplib.onProgress((pos) => {
   showElements();
-})
+});
 
 let actualDur = 0;
 chiplib.onMetadata(async (meta) => {
@@ -101,30 +92,25 @@ chiplib.onMetadata(async (meta) => {
   actualDur = meta.dur.round();
   modTitle.innerText = meta.title || "Untitled";
   modType.innerText = modTypeStr;
-  modDur.innerText = modDurStr
+  modDur.innerText = modDurStr;
   modTracker.innerText = meta.tracker || "Unknown";
   document.title = `NeoPlayer - ${modTitle.innerText} - ${meta.type.toUpperCase()} - ${modDurStr}`;
   modMeta = `Module text/instruments:\n\n${meta.message.split('\n').map((line, i) => `${(i + 1).toString().padStart(2, '0')}: ${line}`).join('\n')}` || "No text/instruments found.";
 });
 
 async function loadModule(url) {
-  // check if the URL is a ModArchive page instead of a direct link
   if (url.includes(modulePage1 || modulePage2 || modulePage3)) {
     const id = url.match(/(\d+)$/);
     await chiplib.load(`${apiDownload}${id[0]}`);
     return;
+  } else if (isNaN(url)) {
+    await chiplib.load(url);
+    return;
   } else {
-    // check if the URL is a direct link to a module, instead a ModArchive module ID
-    if (isNaN(url) === true) {
-      await chiplib.load(url);
-      return;
-    } else {
-      // assume that it is a ModArchive module ID
-      await chiplib.load(`${apiDownload}${url}`);
-      return;
-    };
-  };
-};
+    await chiplib.load(`${apiDownload}${url}`);
+    return;
+  }
+}
 
 play.addEventListener("click", () => {
   if (url.value === "") {
@@ -132,22 +118,22 @@ play.addEventListener("click", () => {
     return;
   } else {
     loadModule(url.value);
-  };
+  }
 });
 
 document.body.onkeyup = function (btn) {
-  if (btn.key == " " || btn.code == "Space") {
+  if (btn.key === " " || btn.code === "Space") {
     chiplib.togglePause();
-  };
+  }
 };
 
 playBtn.addEventListener("click", () => {
-  chiplib.togglePause()
+  chiplib.togglePause();
 });
 
 loopToggle.addEventListener("click", () => {
-  value = value === 0 ? -1 : 0;
-  chiplib.setRepeatCount(value);
+  loopState = loopState === 0 ? -1 : 0;
+  chiplib.setRepeatCount(loopState);
 });
 
 moduleMsgBtn.addEventListener("click", () => {
@@ -169,4 +155,11 @@ closeBtn.addEventListener('click', () => {
 stopBtn.addEventListener("click", () => {
   hideElements();
   chiplib.stop();
+});
+
+// play when user hits enter in input
+url.addEventListener("keydown", (event) => {
+  if (event.key === "Enter") {
+    play.click();
+  }
 });
