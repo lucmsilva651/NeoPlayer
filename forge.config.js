0d2c5331397916aefebcc66e1c2952b142a3560e
Luquinhas <lucmsilva651@gmail.com>
2025-06-16 16:58:46 -0300

    Experimental forge support

---
+++ forge.config.js
@@ -0,0 +1,72 @@
+/** @type {import('electron-forge').ForgeConfig} */
+export default {
+  packagerConfig: {
+    asar: true,
+    icon: 'app/icons/win/icon',
+    appBundleId: 'com.lucmsilva.neoplayer',
+    appCategoryType: 'public.app-category.music',
+    "win32metadata": {
+      "CompanyName": "Lucas Gabriel (lucmsilva)",
+      "ProductName": "NeoPlayer"
+    }
+  },
+  publishers: [
+    {
+      name: '@electron-forge/publisher-github',
+      config: {
+        repository: {
+          owner: 'lucmsilva651',
+          name: 'NeoPlayer'
+        },
+        prerelease: true
+      }
+    }
+  ],
+  makers: [
+    {
+      name: '@electron-forge/maker-squirrel',
+      config: {
+        name: 'NeoPlayer',
+        setupIcon: 'app/icons/win/icon.ico'
+      }
+    },
+    {
+      name: '@electron-forge/maker-deb',
+      config: {
+        options: {
+          icon: 'app/icons/png',
+          category: 'Audio'
+        }
+      }
+    },
+    {
+      name: '@electron-forge/maker-rpm',
+      config: {
+        options: {
+          icon: 'app/icons/png',
+          category: 'Audio'
+        }
+      }
+    },
+    {
+      name: '@electron-forge/maker-dmg',
+      config: {
+        format: 'ULFO',
+        icon: 'app/icons/mac/icon.icns'
+      }
+    },
+    {
+      name: '@electron-forge/maker-zip',
+      platforms: ['darwin']
+    }
+  ],
+  plugins: [
+    {
+      name: '@electron-forge/plugin-auto-unpack-natives',
+      config: {}
+    }
+  ],
+  hooks: {},
+  publishers: [],
+  buildIdentifier: 'com.lucmsilva.neoplayer'
+};
--- main.js
+++ main.js
@@ -1,59 +1,82 @@
-import { app, BrowserWindow, Menu, dialog, ipcMain } from "electron";
+import { app, BrowserWindow, dialog, ipcMain } from "electron";
+import { updateElectronApp } from "update-electron-app";
+import { createRequire } from 'module';
 import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-app.commandLine.appendSwitch('autoplay-policy', 'no-user-gesture-required');
+updateElectronApp();
 
-app.whenReady().then(() => {
-  const win = new BrowserWindow({
-    icon: join(__dirname, 'app', 'icons', 'png', '16x16.png'),
-    width: 600,
-    height: 365,
-    center: true,
-    minWidth: 600,
-    minHeight: 365,
-    visible: false,
-    darkTheme: true,
-    backgroundColor: '#0d0d0d',
-    webPreferences: {
-      preload: join(__dirname, 'preload.js'),
-      nodeIntegration: false,
-      contextIsolation: true,
-      spellcheck: false,
-      sandbox: true
-    }
-  });
+const require = createRequire(import.meta.url);
 
-  win.removeMenu();
-  win.loadFile(join(__dirname, 'app', 'app.html'));
+if (require('electron-squirrel-startup')) {
+  app.quit();
+}
 
-  win.on('show', () => {
-    setTimeout(() => {
+let win = null;
+const gotTheLock = app.requestSingleInstanceLock();
+
+if (!gotTheLock) {
+  app.quit();
+} else {
+  app.on('second-instance', (event) => {
+    if (win) {
+      if (win.isMinimized()) win.restore();
       win.focus();
-    }, 200);
-  });
-
-  win.show();
-
-  app.on("activate", () => {
-    if (BrowserWindow.getAllWindows().length === 0) createWindow();
-  });
-
-  app.on("window-all-closed", () => {
-    if (process.platform !== "darwin") app.quit();
-  });
-
-  let isDialogOpen = false;
-  ipcMain.handle('dialog', async (event, options) => {
-    if (isDialogOpen) return;
-    isDialogOpen = true;
-    try {
-      return await dialog.showMessageBox(options);
-    } finally {
-      isDialogOpen = false;
     };
   });
-});
+
+  app.commandLine.appendSwitch('autoplay-policy', 'no-user-gesture-required');
+  app.whenReady().then(() => {
+    const win = new BrowserWindow({
+      icon: join(__dirname, 'app', 'icons', 'png', '16x16.png'),
+      width: 600,
+      height: 365,
+      center: true,
+      minWidth: 600,
+      minHeight: 365,
+      visible: false,
+      darkTheme: true,
+      backgroundColor: '#0d0d0d',
+      webPreferences: {
+        preload: join(__dirname, 'preload.js'),
+        nodeIntegration: false,
+        contextIsolation: true,
+        spellcheck: false,
+        sandbox: true
+      }
+    });
+
+    win.removeMenu();
+    win.loadFile(join(__dirname, 'app', 'app.html'));
+
+    win.on('show', () => {
+      setTimeout(() => {
+        win.focus();
+      }, 200);
+    });
+
+    win.show();
+
+    app.on("activate", () => {
+      if (BrowserWindow.getAllWindows().length === 0) createWindow();
+    });
+
+    app.on("window-all-closed", () => {
+      if (process.platform !== "darwin") app.quit();
+    });
+
+    let isDialogOpen = false;
+    ipcMain.handle('dialog', async (event, options) => {
+      if (isDialogOpen) return;
+      isDialogOpen = true;
+      try {
+        return await dialog.showMessageBox(options);
+      } finally {
+        isDialogOpen = false;
+      };
+    });
+  });
+};
