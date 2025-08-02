adf4894a6b25f4d3795a27cc2a858ed9a8ac76ae
Luquinhas <lucmsilva651@gmail.com>
2025-08-02 15:08:17 -0300

    Add file type validation (fixes #14)

--- src/js/index.js
+++ src/js/index.js
@@ -196,6 +196,7 @@ element("url").addEventListener("keydown", (event) => {
 });
 
 elements("openfile-btn").forEach((btn) => {
+  element("fileInput").setAttribute("accept", ".mptm, .mod, .s3m, .xm, .it, .667, .669, .amf, .ams, .c67, .cba, .dbm, .digi, .dmf, .dsm, .dsym, .dtm, .etx, .far, .fc, .fc13, .fc14, .fmt, .fst, .ftm, .imf, .ims, .ice, .j2b, .m15, .mdl, .med, .mms, .mt2, .mtm, .mus, .nst, .okt, .plm, .psm, .pt36, .ptm, .puma, .rtm, .sfx, .sfx2, .smod, .st26, .stk, .stm, .stx, .stp, .symmod, .tcb, .gmc, .gtk, .gt2, .ult, .unic, .wow, .xmf, .gdm, .mo3, .oxm, .umx, .xpk, .ppm, .mmcmp");
   btn.addEventListener("click", () => {
     element("fileInput").click();
   });
