e4ba23504fac6334478b741c4d5738aeae4c1470
Luquinhas <lucmsilva651@gmail.com>
2026-03-28 03:35:51 -0300

    Refactor .gitignore and remove Vite build files

--- .vite/build/preload.js
+++
@@ -1,4 +0,0 @@
-//#region src/electron/preload.js
-var { contextBridge, ipcRenderer } = require("electron");
-contextBridge.exposeInMainWorld("api", { alert: (options) => ipcRenderer.invoke("dialog", options) });
-//#endregion
