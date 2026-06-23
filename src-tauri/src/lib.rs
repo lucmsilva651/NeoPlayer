e7f392c425a6d349e68346448979cb1fb1e55587
Luquinhas <lucmsilva651@gmail.com>
2026-06-23 11:25:04 -0300

    Add support to single instance mode

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,7 +1,16 @@
-#[cfg_attr(mobile, tauri::mobile_entry_point)]
+use tauri::{Manager};
+
 pub fn run() {
-    tauri::Builder::default()
-        .plugin(tauri_plugin_prevent_default::init())
+    let mut builder = tauri::Builder::default();
+    #[cfg(desktop)]
+    {
+        builder = builder.plugin(tauri_plugin_single_instance::init(|app, _args, _cwd| {
+            let _ = app.get_webview_window("main")
+                       .expect("no main window")
+                       .set_focus();
+        }));
+    }
+    builder
         .plugin(tauri_plugin_opener::init())
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
